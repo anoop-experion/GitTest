dfa8e38cc4e6bb2138350a1126498e64e44cb062
Anoop R <anoop.r@experionglobal.com>
2015-07-06 16:23:45 +0530

    com1

--- app/src/main/java/com/example/anoopr/gittest/MainActivity.java
+++ app/src/main/java/com/example/anoopr/gittest/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(getApplicationContext(),"Hii",Toast.LENGTH_LONG).show();
+        Log.e("aa","Support");
 
     }
 
