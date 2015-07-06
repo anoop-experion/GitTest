52cc8dccf60ae3c2044d677830d01e03ecd5f2c6
Anoop R <anoop.r@experionglobal.com>
2015-07-06 15:15:13 +0530

    com

--- app/src/main/java/com/example/anoopr/gittest/MainActivity.java
+++ app/src/main/java/com/example/anoopr/gittest/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("Test","Test Support");
+        Log.d("Test","Test Support1");
     }
 
     @Override
